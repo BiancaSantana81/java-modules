9f4119baff45d38108888d54e3a7a6d0ade80d64
bsantana <biancasantana81@hotmail.com>
2024-12-09 17:10:44 -0300

    done class Megaphone: add MegaphoneTest.java

--- module-00/ex00/src/Megaphone.java
+++ module-00/ex00/src/Megaphone.java
@@ -7,7 +7,22 @@ public class Megaphone {
   /**
    * main class.
    */
+  @SuppressWarnings("checkstyle:NeedBraces")
   public static void main(String[] args) {
 
+    // É interessante destacar que a JVM não manda o executável como argumento.
+    // Logo [.jar] aqui não será argumento (como seria em C e C++)
+
+    int numbersArgs = args.length;
+
+    if (numbersArgs == 0) {
+      System.out.println("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+      return;
+    } else {
+      for (String arg : args) {
+        System.out.print(arg.toUpperCase() + " ");
+      }
+      System.out.println("\n");
+    }
   }
 }
