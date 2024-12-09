f01d6301795f6e388107412f3e469a0fa1a83ab4
bsantana <biancasantana81@hotmail.com>
2024-12-09 15:41:42 -0300

    code ex00

--- module-00/ex00/src/Megaphone.java
+++ module-00/ex00/src/Megaphone.java
@@ -1,1 +1,13 @@
-// estruturar
+/**
+ * Receber uma ‘string’ como argumento e a retornar em letras maiúsculas.
+ * Caso nenhuma ‘string’ seja passada como argumento ele deve retornar:
+ * '* LOUD AND UNBEARABLE FEEDBACK NOISE *'
+ */
+public class Megaphone {
+  /**
+   * main class.
+   */
+  public static void main(String[] args) {
+
+  }
+}
