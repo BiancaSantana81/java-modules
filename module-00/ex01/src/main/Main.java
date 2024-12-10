c274cef18174da625d3abc80602296f3e1950128
bsantana <biancasantana81@hotmail.com>
2024-12-10 18:14:08 -0300

    phonebook class: add methods and switch-case in main

--- module-00/ex01/src/main/Main.java
+++ module-00/ex01/src/main/Main.java
@@ -1,8 +1,48 @@
 package main;
 
-public class Main {
-  public static void main(String[] args) {
+import java.util.Scanner; // -> para pegar input do usuário
 
-    Contact contact = new Contact();
+public class Main {
+
+    // Cores ANSI
+    public static final String RESET = "\033[0m";
+    public static final String WHITE = "\033[1;37m";
+    public static final String PINK = "\033[1;35m";
+    public static final String RED = "\033[1;31m";
+    public static final String GREEN = "\033[1;32m";
+    public static final String YELLOW = "\033[1;33m";
+    public static final String BLUE = "\033[1;34m";
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in); // recebe a entrada do usuário
+    String input; // captura a entrada para tratamento.
+
+    PhoneBook phonebook = new PhoneBook();
+
+    while (true) {
+      System.out.println(PINK + "Welcome to the phone book!");
+      System.out.println("ADD: add new contact");
+      System.out.println("SEARCH: to search for a contact in the list");
+      System.out.println("EXIT: to exit the program");
+      System.out.print("Enter your option here: " + RESET);
+
+      input = scanner.nextLine().trim().toLowerCase();
+
+      switch (input) {
+        case "add":
+          phonebook.addContact();
+          break;
+        case "search":
+          phonebook.searchContact();
+          break;
+        case "exit":
+          System.out.println(BLUE + "Program closed. Until next time!" + RESET);
+          return;
+        default:
+          System.out.println(RED + "Invalid option! try again..." + RESET);
+          continue;
+      }
+    }
   }
 }
--- module-00/ex01/src/main/PhoneBook.java
+++ module-00/ex01/src/main/PhoneBook.java
@@ -2,4 +2,17 @@ package main;
 
 public class PhoneBook {
 
+  /* criação do array de contatos de PhoneBook -
+  * em Java eu preciso usar new para criar um array também. */
+  private Contact[] phoneBook = new Contact[8];
+
+  public void addContact() {
+    System.out.println("adicionar contato");
+    return;
+  }
+
+  public void searchContact() {
+    System.out.println("pesquisar contato");
+    return;
+  }
 }
