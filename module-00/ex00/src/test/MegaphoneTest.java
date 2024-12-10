cf8b1883771d5fe44c63cae2c281f586ed602836
bsantana <biancasantana81@hotmail.com>
2024-12-10 14:52:11 -0300

    finish test in ex00

--- module-00/ex00/src/test/MegaphoneTest.java
+++ module-00/ex00/src/test/MegaphoneTest.java
@@ -24,4 +24,27 @@ public class MegaphoneTest {
     String output = outputStream.toString().trim();
     assertEquals("* LOUD AND UNBEARABLE FEEDBACK NOISE *", output);
   }
+  @Test
+  public void testUpperCase() {
+
+    // reconfigurar a saída padrão para aceitar os argumentos que eu passarei aqui no teste.
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream originalOut = System.out;
+    System.setOut(new PrintStream(outputStream));
+
+    String args[] = {"shhhhh... I think the students are asleep...",
+      "Damnit ", "!", "Sorry students, I thought this thing was off."};
+
+    Megaphone.main(args);
+
+    // Restaurar a saída padrão
+    System.setOut(originalOut);
+
+    // Capturar a saída e verificar
+    String output = outputStream.toString().trim();
+    String expectedOutput = "SHHHHH... I THINK THE STUDENTS ARE ASLEEP... DAMNIT  ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.";
+
+    // Verificar se a saída está correta
+    assertEquals(expectedOutput, output);
+  }
 }
