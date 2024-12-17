8fd4e1ef3a483909a449547dac85cd94cad15d1d
bsantana <biancasantana81@hotmail.com>
2024-12-17 13:20:48 -0300

    finish ex00: add JUnit tests

--- module-01/ex00/src/Main.java
+++ module-01/ex00/src/Main.java
@@ -2,6 +2,37 @@ import main.*;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Word!");
+
+    // Fight history...
+    ClapTrap nuru = new ClapTrap("Deka");
+    ClapTrap firstborn = new ClapTrap("Melanis");
+
+    nuru.attack(firstborn.getName());
+    firstborn.takeDamage(5);
+
+    System.out.println("Melanis was furious with Deka for her boldness...");
+
+    firstborn.attack(nuru.getName());
+    nuru.takeDamage(6);
+
+    nuru.seePoints();
+    firstborn.seePoints();
+
+    firstborn.beRepaired(5);
+    nuru.beRepaired(6);
+
+    for (int i = 0; i < 9; i++) {
+      nuru.attack(firstborn.getName());
+      firstborn.takeDamage(1);
+    }
+
+    System.out.println("Nuru, born from the blood of the goddesses, capable of killing one of the war queens!");
+
+    nuru.attack(firstborn.getName());
+    firstborn.takeDamage(1);
+
+    firstborn.seePoints();
+
+    System.out.println("The golden goddesses will pay me!");
   }
 }
