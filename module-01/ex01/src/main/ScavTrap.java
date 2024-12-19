170e49da1cc98a644e816d3e762b19c3277d70a2
bsantana <biancasantana81@hotmail.com>
2024-12-19 17:27:05 -0300

    finish ex01: add tests

--- module-01/ex01/src/main/ScavTrap.java
+++ module-01/ex01/src/main/ScavTrap.java
@@ -5,6 +5,7 @@ public class ScavTrap extends ClapTrap {
   public static final String YELLOW = "\033[1;33m";
   public static final String RESET = "\033[0m";
 
+ // O construtor padrão foi reescrito aqui, pois em Java as classes filhas precisam ter seus próprios construtores caso eu não queira usar o default
   public ScavTrap (String name) {
     super(name);
     this.hitPoints = 100;
